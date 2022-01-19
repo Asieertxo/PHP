096be887e537c4af029e54be64a3c614e013c976
Asieertxo <asgal.gl@gmail.com>
2022-01-19 17:46:54 +0100

    Nuevos ejercicios de PHP

---
+++ Tema16/Ejercicio1/Libros.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="./css/estilos.css">
+</head>
+<body>
+
+
+<?php
+$SERVERNAME = "localhost";
+$USERNAME = "root";
+$PASSWORD = "";
+$DBNAME = "libros";
+
+//Crear conexion, le pongo @ ya que despues compruebo si la conexion se ha realizaqdo bien
+@$conection = mysqli_connect($SERVERNAME, $USERNAME, $PASSWORD, $DBNAME);
+
+
+//Comprobar la conexion la conexion
+if (!$conection) {
+    echo "<h2 class='rojo'>Error de conexion: </h1>" . mysqli_connect_error();
+    die();
+}else{
+    echo "<h2 class='verde'>Conexion satisfactoria</h1>";
+}
+
+//determinamos los limites de la sentencia SQL
+$limit = 0;
+if(!empty($_GET['pag'])){
+    $pag = $_GET['pag'];
+}else{
+    $pag = 1;
+}
+$limit = $limit + (($pag -1) * 10);
+
+//vemos cuantas filas tiene en total
+$sql = "SELECT id, isbn, title, author, stock, price FROM book";
+$result = mysqli_query($conection, $sql);
+$n_rows = mysqli_num_rows($result);
+
+//sacamos solo 10 dependiendo del limit
+$sql_limit = "SELECT id, isbn, title, author, stock, price FROM book LIMIT $limit,10";
+$result_limit = mysqli_query($conection, $sql_limit);
+
+if ($n_rows > 0) {
+    //pinta las filas sacadas con el limit
+    echo "<div class='contenedor'>";
+    echo "<table class='tabla'>";
+    while($row = mysqli_fetch_assoc($result_limit)) {
+        echo "<tr>";
+            echo "<td><b>ID:</b> $row[id]</td>   ";
+            echo "<td><b>ISBN:</b> $row[isbn]   </td>";
+            echo "<td><b>Title:</b> $row[title]   </td>";
+            echo "<td><b>Author:</b> $row[author]   </td>";
+            echo "<td><b>Stock:</b> $row[stock]   </td>";
+            echo "<td><b>Price:</b> $row[price]</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+
+    //determina cuantas paginas hay que añadir dependiendo de las filas totales
+    if($n_rows > 10){
+        $n_pag = 1;
+        echo "<div class='paginas'>";
+        echo "<a href='./libros.php?pag=$n_pag'>$n_pag</a>";
+        $n_rows = $n_rows -10;
+        $n_pag = $n_pag + 1;
+        do{
+            echo "<a href='./libros.php?pag=$n_pag'>$n_pag</a>";
+            $n_rows = $n_rows -10;
+            $n_pag = $n_pag + 1;
+        }while($n_rows > 0);
+        echo "</div>";
+        
+    }
+    echo "</div>";
+}else{
+    echo "0 results";
+}
+
+mysqli_close($conection);
+?>
+
+</body>
---
+++ Tema16/Ejercicio1/Libros.sql
@@ -0,0 +1,261 @@
+/*
+
+Abajo esta comentado el escript de creacion manual, no el exportado
+
+*/
+
+
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 12-01-2022 a las 10:01:32
+-- Versión del servidor: 5.5.24-log
+-- Versión de PHP: 5.4.3
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `libros`
+--
+
+DROP DATABASE IF EXISTS `libros`;
+CREATE DATABASE `libros`;
+USE DATABASE `libros`;
+
+
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `book`
+--
+
+CREATE TABLE IF NOT EXISTS `book` (
+  `id` int(10) NOT NULL DEFAULT '0',
+  `isbn` varchar(13) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `author` varchar(255) DEFAULT NULL,
+  `stock` smallint(5) DEFAULT NULL,
+  `price` float DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `book`
+--
+
+INSERT INTO `book` (`id`, `isbn`, `title`, `author`, `stock`, `price`) VALUES
+(1, '9788419108754', 'Libro1', 'Miguel', 20, 25),
+(2, '9788465408754', 'Libro2', 'Antonio', 10, 15),
+(3, '5364721956847', 'Libro3', 'Juan', 15, 20),
+(4, '632141956847', 'Libro4', 'Pedro', 10, 10),
+(5, '632141286477', 'Libro5', 'Arturo', 25, 5),
+(6, '9784419108754', 'Libro6', 'Angel', 24, 23),
+(7, '9783465408754', 'Libro7', 'Carmen', 16, 12),
+(8, '5364721756847', 'Libro8', 'Alberto', 12, 21),
+(9, '632141996847', 'Libro9', 'Alvaro', 18, 14),
+(10, '632141206477', 'Libro10', 'Fernando', 23, 53),
+(11, '9788419378754', 'Libro11', 'Guillermo', 20, 27),
+(12, '9128465408754', 'Libro12', 'Pedro', 10, 19),
+(13, '8367721956847', 'Libro13', 'Carla', 15, 28),
+(14, '3243243432434', 'Libro14', 'Luis', 10, 13),
+(15, '2343243243244', 'Libro15', 'Ivan', 24, 9),
+(16, '3465789654653', 'Libro16', 'Ana', 22, 21),
+(17, '9873268764893', 'Libro17', 'Diego', 11, 11),
+(18, '5364321956847', 'Libro18', 'Gonzalo', 18, 22),
+(19, '632141957847', 'Libro19', 'Alba', 16, 17),
+(20, '632141266477', 'Libro20', 'Lucia', 23, 3),
+(21, '9788415108754', 'Libro21', 'Maria', 20, 30),
+(22, '9788464408754', 'Libro22', 'Sara', 17, 12),
+(23, '5364731956847', 'Libro23', 'Tania', 13, 22),
+(24, '632141256847', 'Libro24', 'Angela', 32, 18),
+(25, '632141281477', 'Libro25', 'Paula', 24, 37),
+(26, '9788419138754', 'Libro26', 'Elena', 31, 38),
+(27, '8932746767833', 'Libro27', 'Rebeca', 30, 32),
+(28, '5364741956847', 'Libro28', 'Irene', 5, 22),
+(29, '632142356847', 'Libro29', 'Blanca', 7, 17),
+(30, '632173586477', 'Libro30', 'Pablo', 26, 6),
+(31, '832745578666', 'Libro31', 'Rafa', 14, 15),
+(32, '984375687748', 'Libro32', 'Ruben', 11, 10);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `borrowed_books`
+--
+
+CREATE TABLE IF NOT EXISTS `borrowed_books` (
+  `costumer_id` int(10) NOT NULL DEFAULT '0',
+  `book_id` int(10) NOT NULL DEFAULT '0',
+  `empezar` datetime NOT NULL,
+  `fin` datetime DEFAULT NULL,
+  PRIMARY KEY (`costumer_id`,`book_id`,`empezar`),
+  KEY `book_id` (`book_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `costumer`
+--
+
+CREATE TABLE IF NOT EXISTS `costumer` (
+  `id` int(10) NOT NULL DEFAULT '0',
+  `firstname` varchar(255) DEFAULT NULL,
+  `surname` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `type` enum('basic','premium') DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sale`
+--
+
+CREATE TABLE IF NOT EXISTS `sale` (
+  `id` int(10) NOT NULL DEFAULT '0',
+  `costumer_id` int(10) DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sale_book`
+--
+
+CREATE TABLE IF NOT EXISTS `sale_book` (
+  `book_id` int(10) NOT NULL DEFAULT '0',
+  `sale_id` int(10) NOT NULL DEFAULT '0',
+  `amount` smallint(5) DEFAULT NULL,
+  PRIMARY KEY (`book_id`,`sale_id`),
+  KEY `sale_id` (`sale_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `borrowed_books`
+--
+ALTER TABLE `borrowed_books`
+  ADD CONSTRAINT `borrowed_books_ibfk_1` FOREIGN KEY (`costumer_id`) REFERENCES `costumer` (`id`),
+  ADD CONSTRAINT `borrowed_books_ibfk_2` FOREIGN KEY (`book_id`) REFERENCES `book` (`id`);
+
+--
+-- Filtros para la tabla `sale_book`
+--
+ALTER TABLE `sale_book`
+  ADD CONSTRAINT `sale_book_ibfk_1` FOREIGN KEY (`book_id`) REFERENCES `book` (`id`),
+  ADD CONSTRAINT `sale_book_ibfk_2` FOREIGN KEY (`sale_id`) REFERENCES `sale` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+
+
+
+
+
+/*
+CREATE DATABASE libros;
+
+USE DATABASE libros;
+
+CREATE TABLE book (
+	id		int(10),
+        isbn		varchar(13),
+        title		varchar(255),
+        author		varchar(255),
+        stock		smallint(5),
+        price		float,
+        
+        PRIMARY KEY (id)
+);
+
+INSERT INTO `book` (`id`, `isbn`, `title`, `author`, `stock`, `price`) VALUES
+(1, '9788419108754', 'Libro1', 'Miguel', 20, 25),
+(2, '9788465408754', 'Libro2', 'Antonio', 10, 15),
+(3, '5364721956847', 'Libro3', 'Juan', 15, 20),
+(4, '6321419568475', 'Libro4', 'Pedro', 10, 10),
+(5, '6321412864772', 'Libro5', 'Arturo', 25, 5),
+(6, '6321413454474', 'Libro6', 'David', 15, 10),
+(7, '6321345454347', 'Libro7', 'Ivan', 30, 5),
+(8, '6321345354147', 'Libro8', 'Ana', 20, 15),
+(9, '5476123956847', 'Libro9', 'Patricia', 10, 11),
+(10, '2147895625365', 'Libro10', 'Sara', 12, 12),
+(11, '6143254356847', 'Libro11', 'Carlos', 14, 25),
+(12, '6235435689947', 'Libro12', 'Alvaro', 16, 35),
+(13, '9812734569847', 'Libro13', 'Asier', 18, 33),
+(14, '0923485600984', 'Libro14', 'Juanjo', 9, 28),
+(15, '9876344898974', 'Libro15', 'Jose', 5, 17),
+(16, '7623487348993', 'Libro16', 'Maria', 0, 16),
+(17, '7893489049499', 'Libro17', 'Claudia', 2, 15),
+(18, '6893490849404', 'Libro18', 'Tania', 26, 12),
+(19, '5436576677179', 'Libro19', 'Sofia', 25, 21),
+(20, '0578567145664', 'Libro20', 'Lucas', 25, 11),
+(21, '1234565797547', 'Libro21', 'Fernando', 15, 17),
+(22, '8768923451452', 'Libro22', 'Patricio', 5, 26),
+(23, '6789085745433', 'Libro23', 'Nora', 0, 24),
+(24, '9874994837569', 'Libro24', 'Lucia', 14, 28),
+(25, '8473569876344', 'Libro25', 'Antonio', 18, 16),
+(26, '9283475698476', 'Libro26', 'Jaime', 17, 13),
+(27, '9384948375698', 'Libro27', 'Elena', 10, 22),
+(28, '8473658976784', 'Libro28', 'Angela', 20, 34),
+(29, '8976458748490', 'Libro29', 'Gonzalo', 25, 31),
+(30, '9874365897487', 'Libro30', 'Alberto', 30, 5);
+
+CREATE TABLE costumer (
+	id		int(10),
+        firstname	varchar(255),
+        surname		varchar(255),
+        email		varchar(255),
+        type		enum('basic', 'premium'),
+        
+        PRIMARY KEY (id)
+);
+
+CREATE TABLE borrowed_books (
+	costumer_id	int(10),
+        book_id		int(10),
+        start		datetime,
+        end		datetime,
+        
+        PRIMARY KEY (costumer_id, book_id, start),
+        FOREIGN KEY (costumer_id) REFERENCES costumer(id),
+        FOREIGN KEY (book_id) REFERENCES book(id)
+);
+
+CREATE TABLE sale (
+	id		int(10),
+        costumer_id	int(10),
+        date		datetime,
+       	
+        PRIMARY KEY (id)
+);
+
+CREATE TABLE sale_book (
+	book_id		int(10),
+        sale_id		int(10),
+        amount		smallint(5),
+        
+        PRIMARY KEY (book_id, sale_id),
+        FOREIGN KEY (book_id) REFERENCES book(id),
+        FOREIGN KEY (sale_id) REFERENCES sale(id)
+);
+*/
