bb4a54b4eef4f56bf5417bcdfbc3719d38408eff
usuario generico daw <daw@ieszayas.org>
2022-03-02 09:06:11 +0100

    two if conection

--- EXAMEN 2º/ejercicio 2/php/conection.php
+++ EXAMEN 2º/ejercicio 2/php/conection.php
@@ -3,17 +3,23 @@
 function conection(){
     $SERVERNAME = "localhost";
     $USERNAME = "root";
-    $PASSWORD = "root";
+    $PASSWORD1 = "";
+    $PASSWORD2 = "root";
     $DBNAME = "inmobiliaria";
 
     //Crear conexion, le pongo @ ya que despues compruebo si la conexion se ha realizaqdo bien
-    @$conection = mysqli_connect($SERVERNAME, $USERNAME, $PASSWORD, $DBNAME);
+    @$conection = mysqli_connect($SERVERNAME, $USERNAME, $PASSWORD1, $DBNAME);
 
 
     //Comprobar la conexion la conexion
     if (!$conection) {
-        echo "<h1 class='rojo'>Error de conexion: </h1>" . mysqli_connect_error();
-        die();
+        @$conection = mysqli_connect($SERVERNAME, $USERNAME, $PASSWORD2, $DBNAME);
+        if (!$conection){
+            echo "<h1 class='rojo'>Error de conexion: </h1>" . mysqli_connect_error();
+            die();
+        }else{
+            echo "<p class='verde'>Conexion satisfactoria</p>";
+        }
     }else{
         echo "<p class='verde'>Conexion satisfactoria</p>";
     }
