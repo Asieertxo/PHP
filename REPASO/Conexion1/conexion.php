1d18f74dbc66473d4b959b3796f4bd673ce20680
Asieertxo <asgal.gl@gmail.com>
2022-05-02 19:16:41 +0200

    Conexion y crud con PDO avanzado

--- REPASO/Conexion1/conexion.php
+++ REPASO/Conexion1/conexion.php
@@ -2,16 +2,58 @@
 
 
 
-
 class conexion{
+    private $user = "root";
+    private $pass = "root";
+    private $dbname = "libros";
+    private $server = "localhost";
+
+    private $pdo;
+    private $sql;
+    private $resultado;
+
+    function conectar(){
+        try{
+            $this->pdo = new PDO("mysql:host=$this->server; dbname=$this->dbname", $this->user, $this->pass);
+            echo "Conexion correcta </br></br>";
+        }catch(PDOException $error){
+            echo "No se conecto" . $error->getMessage();
+        }
+    }
+
+    function accion($sql){
+        try{
+            $this->sql = $sql;
+            $this->resultado = $this->pdo->prepare($this->sql);
+            $this->resultado->execute();
+
+            return $this->resultado;
+        }catch(PDOException $error){
+            die("Error en la accion ---- " . $error->getMessage());
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*class conexion{
     private $user = "root";
     private $pass = "root";
     private $dbname = "inmobiliaria";
     private $server = "localhost";
 
     function conectar(){
-
-        $busqueda = "piso";
+        $busqueda1 = "piso";
+        $busqueda2 = "Pozuelo";
 
         try{
             $pdo = new PDO("mysql:host=$this->server; dbname=$this->dbname", $this->user, $this->pass);
@@ -19,22 +61,18 @@ class conexion{
 
             $pdo->exec("SET CHARACTER SET utf8");
 
-            $sql = "SELECT tipo FROM viviendas WHERE tipo = ?";
+            $sql = "SELECT tipo, zona FROM viviendas WHERE tipo = :tipo AND zona = :zona";
             $resultado = $pdo->prepare($sql);
-            $resultado->execute(array($busqueda));
+            $resultado->execute(array(":tipo"=>$busqueda1, ":zona"=>$busqueda2));
 
             while($registro = $resultado->fetch(PDO::FETCH_ASSOC)){
-                echo "Tipo " . $registro['tipo']. "</br>";
+                echo "Tipo " . $registro['tipo'] . ", zona " . $registro['zona'] .   "</br>";
             }
             $resultado->closeCursor();
+            
         }catch(PDOException $error){
             echo "No se conecto" . $error->getMessage();
         }
-        
     }
-}
-
-$conexion = new conexion();
-$conexion->conectar();
-
-?>
+}*/
+?> 
---
+++ REPASO/Conexion1/index.php
@@ -0,0 +1,55 @@
+<?php
+function autoload($clase){
+    require "./" . $clase . ".php";
+}
+spl_autoload_register('autoload');
+
+
+$conexion = new conexion();
+$conexion->conectar();
+
+
+if(isset($_GET['boton'])){
+    if($_GET['boton'] == "select"){
+        select($conexion);
+    }elseif($_GET['boton'] == "insert"){
+        insert($conexion);
+    }elseif($_GET['boton'] == "delete"){
+        delete($conexion);
+    }
+}else{
+    echo "<a href='./index.php?boton=select'>Select</a> </br>";
+    echo "<a href='./index.php?boton=insert'>Insert</a> </br>";
+    echo "<a href='./index.php?boton=delete'>Delete</a> </br>";
+}
+
+
+
+function select($conexion){
+    $sql = "SELECT id, isbn, title, author, stock, price FROM book";
+    $result = $conexion->accion($sql);
+
+    while($registro = $result->fetch(PDO::FETCH_ASSOC)){
+        echo "isbn " . $registro['isbn'] . " --- title " . $registro['title'] .   "</br>";
+    }
+    $result->closeCursor();
+}
+
+function insert($conexion){
+    $sql = "INSERT INTO `book` ( `isbn`, `title`, `author`, `stock`, `price`) VALUES ('4828475406254', 'Libro33', 'Pedro', 1, 30)";
+    $result = $conexion->accion($sql);
+
+    if($result){
+        echo "Libro subido con exito";//asi podemos meter las variables mas facil que sacando el texto desde la clase conexion
+    }//si falla ya da error el catch
+}
+
+function delete($conexion){
+    $sql = "DELETE FROM `book` WHERE `id`= 37";
+    $result = $conexion->accion($sql);
+
+    if($result){
+        echo "Libro borrado con exito";//da exito aun cuando borra un libro que no existia
+    }
+}
+?>
