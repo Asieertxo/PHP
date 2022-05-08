d8a245913fd361b32fc832b0c523d8610a228492
Asier Garcia <asgal.gl@gmail.com>
2022-05-08 12:21:57 +0200

    solo acepta unas extensiones

--- REPASO/Upload/index.php
+++ REPASO/Upload/index.php
@@ -3,7 +3,7 @@
 echo <<<EOD
     <h1>Subir un fichero</h1>
     <form action="./subir.php" method="POST" enctype="multipart/form-data">
-        <input type="file" name="fichero">Fichero</br>
+        <input type="file" name="fichero" accept=".txt">Fichero</br>
         <input type="submit" name"enviar" value="enviar" />
     </form>
 EOD;
