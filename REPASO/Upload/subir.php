800d57908b57bd555e54d02829b70dafa9807144
Asier Garcia <asgal.gl@gmail.com>
2022-05-08 12:14:47 +0200

    subida de ficheros terminada

--- REPASO/Upload/subir.php
+++ REPASO/Upload/subir.php
@@ -3,16 +3,18 @@
 if(is_uploaded_file($_FILES['fichero']['tmp_name'])){
     $directorio = '.\ficheros\\';
     $nombre = $_FILES['fichero']['name'];
-    $fecha = date("d-m-y-H:i:s");
-    $nombreCompleto = $directorio.$nombre."--".$fecha;
+    $fecha = date('Y-m-d-h-h-s');
+    $nombreCompleto = $directorio.$fecha."--".$nombre;
     if(is_dir($directorio)){
         move_uploaded_file ($_FILES['fichero']['tmp_name'],$nombreCompleto);
         echo "Fichero " . $nombreCompleto . " se ha subido correctamente";
     }else{
         echo "Directorio invalido";
+        header("refresh:3; url=index.php");
     }
 }else{
     echo "No se ha subido bien el fichero";
+    header("refresh:3; url=index.php");
 }
 
 ?>
